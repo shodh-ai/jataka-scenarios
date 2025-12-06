e5506f00ff694cc357678767b52407dacb61f530
google <apple@googles-Mac-mini.local>
2025-12-06 18:44:00 +0530

    Setup Level 4: Clean App, no Docker

--- full-stack-ecommerce/src/server.js
+++ full-stack-ecommerce/src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 const db = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Ensure DB is connected before starting the server
 db.connect()
